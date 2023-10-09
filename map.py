f0973db0f87cc5d369976fc43135514b58a58921
Chayadon <100855676+Chayadonn@users.noreply.github.com>
2023-10-10 00:53:47 +0700

    First Commit

---
+++ map.py
@@ -0,0 +1,20 @@
+import pickle
+import warnings
+
+import streamlit as st
+
+import plotly.express as px
+import pandas as pd
+warnings.filterwarnings("ignore")
+
+foo = [{'centroid_lat': 18.8071978, 'centroid_lon':98.9626453, 'car_hours':100 , 'peak_hour':2.8}, 
+       {'centroid_lat': 18.8071978, 'centroid_lon':98.9626453, 'car_hours':1200 , 'peak_hour':2},
+       {'centroid_lat': 18.796642425064054, 'centroid_lon':98.95349937696828, 'car_hours':1700 , 'peak_hour':2.4}]
+
+df = pd.DataFrame(foo)
+print(df)
+
+fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_hour", size="car_hours",
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
+                  mapbox_style="carto-positron")
+fig.show()
