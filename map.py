23511405efb553ae38342e2f53ab1559859c1776
Chayadon <100855676+Chayadonn@users.noreply.github.com>
2023-10-10 01:25:03 +0700

    Update map.py

--- map.py
+++ map.py
@@ -4,8 +4,7 @@ import plotly.express as px
 import pandas as pd
 
 
-
-def map():
+if __name__ == "__main__":
        info = [{'centroid_lat': 18.8071978, 'centroid_lon':98.9626453, 'car_hours':100 , 'peak_hour':2.8}, 
               {'centroid_lat': 18.8071978, 'centroid_lon':98.9626453, 'car_hours':1200 , 'peak_hour':2},
               {'centroid_lat': 18.796642425064054, 'centroid_lon':98.95349937696828, 'car_hours':1700 , 'peak_hour':2.4}]
@@ -14,6 +13,3 @@ def map():
                      color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
                      mapbox_style="carto-positron")
        fig.show()
-
-if __name__ == "__main__":
-       map()
