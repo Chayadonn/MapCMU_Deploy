94a166e80a9b0f995a6785c9c068ac0105553ef6
Chayadon <100855676+Chayadonn@users.noreply.github.com>
2023-10-10 01:42:33 +0700

    Update map.py

--- map.py
+++ map.py
@@ -9,7 +9,13 @@ if __name__ == "__main__":
               {'centroid_lat': 18.8071978, 'centroid_lon':98.9626453, 'car_hours':1200 , 'peak_hour':2},
               {'centroid_lat': 18.796642425064054, 'centroid_lon':98.95349937696828, 'car_hours':1700 , 'peak_hour':2.4}]
        df = pd.DataFrame(info)
+
        fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_hour", size="car_hours",
                      color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
                      mapbox_style="carto-positron")
+       st.plotly_chart(fig)
+
+
+
+
 
