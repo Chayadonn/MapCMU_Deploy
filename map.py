dce1ff5be8d0081960d1b6f5cf9b0108338f3554
Chayadon <100855676+Chayadonn@users.noreply.github.com>
2023-10-10 17:47:46 +0700

    Update map.py

--- map.py
+++ map.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    st.title('purple[CMU] :MAP :sunglasses:')
+    st.title(':purple[CMU] MAP :sunglasses:')
     gate = {'วิศวกรรมศาสตร์': 4174653,
             'แยก อมช': 2898242,
             'แยกบริหาร': 2264162,
